3c50eafbcb2c0bb03c2b9657fe1553b3d45e112d
AugustKupczyk <augustkup@gmail.com>
2023-05-23 23:05:18 -0300

    agregando form para productos

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const rutasMain = require ("./routes/mainRoutes.js");
-const rutasProducto = require ("./routes/productRoutes.js");
-const rutasUser = require ("./routes/userRoutes.js");
-const rutasAdmin = require ("./routes/adminRoutes.js");
+const rutasMain = require("./routes/mainRoutes.js");
+const rutasProducto = require("./routes/productRoutes.js");
+const rutasUser = require("./routes/userRoutes.js");
+const rutasAdmin = require("./routes/adminRoutes.js");
 const path = require("path");
 
 app.set("view engine", "ejs");
-app.set("views",[path.join(__dirname,"./views"), 
-                path.join(__dirname,"./views/users"),
-                path.join(__dirname,"./views/products"),
-                path.join(__dirname,"./views/admin")]);
+app.set("views", [path.join(__dirname, "./views"),
+path.join(__dirname, "./views/users"),
+path.join(__dirname, "./views/products"),
+path.join(__dirname, "./views/admin")]);
 
 app.use(express.static("public"));
 app.use(rutasMain);
-app.use("/products",rutasProducto);
-app.use("/users",rutasUser);
-app.use("/admin",rutasAdmin);
+app.use("/products", rutasProducto);
+app.use("/users", rutasUser);
+app.use("/admin", rutasAdmin);
 
-app.listen(3030,() => console.log("Levantando un servidor con Express, http://localhost:3030/"));
+app.listen(3030, () => console.log("Levantando un servidor con Express, http://localhost:3030/"));
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const adminControllers = require("../controllers/adminControllers.js");
 const router = express.Router();
 
-router.get("agregar-producto", adminControllers.getAgregar);
-router.get("editar-producto", adminControllers.getEditar);
+router.get("/agregar-producto", adminControllers.getAgregar);
+router.get("/editar-producto", adminControllers.getEditar);
 
 module.exports = router;
