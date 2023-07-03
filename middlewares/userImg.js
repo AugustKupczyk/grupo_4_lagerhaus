0df4a6f980aadb623ff6b90220e0b7860d519ed2
AugustKupczyk <augustkup@gmail.com>
2023-07-03 19:59:45 -0300

    multer de register

---
+++ middlewares/userImg.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    
+    destination: (req, file, cb) => {
+        cb(null, './public/imgs/users/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
